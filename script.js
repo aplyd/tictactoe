4cdb002b0447c037a431267d0f847ac4bac293a4
aplyd <austin@ftacnik.com>
2019-10-14 14:28:36 -0700

    render display from gameboard array

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-const Gameboard = (() => {
+const gameBoard = (() => {
     let X = 'X';
     let O = 'O';
-    gameBoard: [X, O, O, X, O, X, X, O, X]; 
+    let gameBoard = [X, O, O, X, O, X, X, O, X]; 
     return {
         gameBoard
     }
 })();
 
 const displayController = (() => {
+    const spaces = document.querySelectorAll('.spaces');
+    const render = () => {
+        for (i = 0; i<spaces.length; i++) {
+            spaces[i].textContent = gameBoard.gameBoard[i];
+            spaces[i].addEventListener('click', function(e) {
+                console.log(e.target);
+            })
+        }
+    }
     return {
-
+        render
     }
 })();
 
-function player(name, score) {
+displayController.render();
+
+const Player = (name, score, currentMove) => {
     return {
         name,
-        score
+        score,
+        currentMove
     }
 }
