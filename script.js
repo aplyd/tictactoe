a22139e822aa613394a304919e1e2f693ea6db20
aplyd <austin@ftacnik.com>
2019-10-16 14:35:33 -0700

    fix multiple rounds and reset problem

--- script.js
+++ script.js
@@ -1,27 +1,56 @@
 const game = (() => {
     const x = 'X';
     const o = 'O';
-    let gameBoard = [, , , 
-                    , , , 
-                    , , ];
+    let gameBoard = ['','','','','','','','',''];
     let counter = 0;
+
+    //TODO: x = 1, o = -1. calculate each direction instead of the below garbage
+    let winnerCheck = () => {
+            if (gameBoard[0] == x && gameBoard[1] == x && gameBoard[2] == x ||
+                gameBoard[3] == x && gameBoard[4] == x && gameBoard[5] == x ||
+                gameBoard[6] == x && gameBoard[7] == x && gameBoard[8] == x ||
+                gameBoard[0] == x && gameBoard[3] == x && gameBoard[6] == x ||
+                gameBoard[1] == x && gameBoard[4] == x && gameBoard[7] == x ||
+                gameBoard[2] == x && gameBoard[5] == x && gameBoard[8] == x ||
+                gameBoard[0] == x && gameBoard[4] == x && gameBoard[8] == x ||
+                gameBoard[2] == x && gameBoard[4] == x && gameBoard[6] == x ) {
+                    displayController.showWinner(user1.name);
+            } else if (gameBoard[0] == x && gameBoard[1] == x && gameBoard[2] == x ||
+                gameBoard[3] == o && gameBoard[4] == o && gameBoard[5] == o ||
+                gameBoard[6] == o && gameBoard[7] == o && gameBoard[8] == o ||
+                gameBoard[0] == o && gameBoard[3] == o && gameBoard[6] == o ||
+                gameBoard[1] == o && gameBoard[4] == o && gameBoard[7] == o ||
+                gameBoard[2] == o && gameBoard[5] == o && gameBoard[8] == o ||
+                gameBoard[0] == o && gameBoard[4] == o && gameBoard[8] == o ||
+                gameBoard[2] == o && gameBoard[4] == o && gameBoard[6] == o) {
+                    displayController.showWinner(user2.name);
+                }
+    }
+
     return {
         gameBoard,
         x,
         o,
+        winnerCheck,
         counter
     }
 })();
+
+
+
 
 const displayController = (() => {
     //get spaces and create array from nodelist
     const spaces = document.querySelectorAll('.spaces');
     let spacesArr = Array.from(spaces);
 
-    //plays each move on selected space
+    //black whole-screen div to announce winner
+    let announceWinner = document.querySelector('#announce-winner');
+    let winnerMessage = document.querySelector('#winner-message');
+
+    //event listener on open spaces to play players selection depending on who's turn it is
     spaces.forEach( element => {
         element.addEventListener('click', (e) => {
-            //checks who's turn it is and then plays on selected space
             if (game.counter % 2 == 0) {
                 user1.currentMove(e.target);
             } else {
@@ -30,20 +59,57 @@ const displayController = (() => {
         })
     })
 
-    //assigns each space players move from gameboard array
+    //takes player moves from gameBoard array and fills in the display
     const renderDisplay = () => {
         for (i = 0; i<spaces.length; i++) {
             spaces[i].textContent = game.gameBoard[i];
         }
+        
+        announceWinner.style.display = 'none';
     }
+
+    //reveals black div to announce winner and calls display reset
+    const showWinner = (winner) => {
+        winnerMessage.textContent = `${winner} wins`;
+        announceWinner.style.display = 'block';
+        reset();
+    }
+
+    const showTie = () => {
+        console.log('show tie ran');
+        winnerMessage.textContent = 'cats game';
+        announceWinner.style.display = 'block';
+        reset();
+    }
+
+    //event listener added to reset display and reset all game functions
+    const reset = () => {
+        announceWinner.addEventListener('click', function() {
+
+            announceWinner.style.display = 'none';
+            
+            for (i = 0; i<game.gameBoard.length; i++) {
+                game.gameBoard[i] = '';
+            }
+
+            game.counter = 0;
+
+            renderDisplay();           
+        })
+    }
+
     return {
         spaces,
         spacesArr,
-        renderDisplay
+        renderDisplay,
+        showWinner,
+        reset,
+        showTie
     }
 })();
 
 displayController.renderDisplay();
+
 
 const Player = (name, xoro) => {
     
@@ -53,7 +119,16 @@ const Player = (name, xoro) => {
         if (!game.gameBoard[clickedIndex]) {
             game.gameBoard[clickedIndex] = xoro;
             displayController.renderDisplay();
-            game.counter++;
+            game.winnerCheck();
+            
+            console.log()
+            if (game.counter > 8) {
+                console.log('over 8');
+                displayController.showTie();
+            } else {
+                console.log('under 8');
+                game.counter++;
+            }
         }
     }
     return {
